4c67ea75840860ef8eb5a2bfb25870615d008abb
Nickita <102178609+manihateu@users.noreply.github.com>
2025-03-24 20:06:14 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Менджер Книг</h1>
+      <h1 className="text-2xl font-bold mb-4">Менеджер книг</h1>
 
       <div className="mb-4">
         <input
